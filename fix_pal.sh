23224edcd58b7a0161ebf2df27ed2ca1f36de8b5
agentspooky <jack@fisherstone.com>
2021-09-05 16:08:32 -0400

    Compatibility updates & minor tweaks:
        - FFmpeg compatibility: allow for audio bitrates that aren't expressed with a decimal.
        - Refuse to run when the input and output paths are identical. (If you're determined, you can easily get around this, but this should help prevent accidents.)
        - Better handling of temp chapter files in the unlikely event that temp paths don't change and temp files don't get cleaned up.

--- fix_pal.sh
+++ fix_pal.sh
@@ -6,6 +6,7 @@
 # usage: $SCRIPT_NAME <infile> <outfile>
 #
 
+
 # If interrupted with CTRL+C, do cleanup
 trap cleanup INT
 
@@ -14,11 +15,13 @@ USAGE="usage: $0 <infile> <outfile>"
 CORRECTION_FACTOR=25/24
 audio_factor=`bc -l <<< "1/($CORRECTION_FACTOR)"`
 
+
 # Validate args
 if [ $# -ne 2 ]; then
 	echo ${USAGE}
 	exit
 fi
+
 
 # Source & dest files
 infile=$1
@@ -26,17 +29,29 @@ outfile=$2
 tmpdir=$(mktemp -d "${TMPDIR:-/var/tmp}/pal-XXXXXXXX") # Thanks to James Ainslie
 tempfile=${tmpdir}/temp.mkv
 
+
 # Clean up the temp directory
 function cleanup {
 	echo "Cleaning up temp files..."
 	rm -rf "${tmpdir}" # Thanks to James Ainslie
 }
 
+
+# If two files are the same, stop.
+# TODO: check for different paths pointing to the same file (e.g. relative vs absolute)
+function check_diff {
+	if [ $1 = $2 ]; then
+		echo 'Input and output files are the same. Stopping.'
+		exit
+	fi
+}
+
+
 # If a given file already exists, ask for confirmation before proceeding.
 function confirm_overwrite {
 	local file=$1
 	if [ -e $file ]; then
-		read -p "Output file \`${outfile}\` already exists.
+		read -p "Output file \`$1\` already exists.
 Do you want to overwrite it? [y|N] " yn
 		case $yn in
 			[Yy]* ) ;;
@@ -44,6 +59,7 @@ Do you want to overwrite it? [y|N] " yn
 		esac
 	fi
 }
+
 
 # Read an arbitrary track file, and produce a new one with timings slowed down.
 # This function modified slightly from code by James Ainslie from
@@ -53,7 +69,7 @@ function edit_timings {
 	local new_track_file=$2
 	local factor=$3
 	local search='[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}.[0-9]\{3\}'
-	touch $new_track_file
+	> $new_track_file
 	while IFS= read -r line || [[ -n $line ]]; do
 		local timestamp=$(echo $line | grep -o $search | cat)
 		if [ "$timestamp" ]; then
@@ -75,6 +91,7 @@ function edit_timings {
 	done < $old_track_file
 }
 
+
 # Pull out the chapter data, then generate a new chapter file with re-timed
 # chapters. This function modified slightly from code by James Ainslie from
 # <https://blog.delx.net.au/2016/05/fixing-pal-speedup-and-how-film-and-video-work/comment-page-1/#comment-100160>
@@ -92,6 +109,7 @@ function fix_chapters {
 	fi
 }
 
+
 # Get all tracks that AREN'T AUDIO (e.g. video and subtitle tracks), then build
 # the `--sync` string for a subsequent `mkvmerge` call.
 function get_sync_flags {
@@ -106,13 +124,15 @@ function get_sync_flags {
 	done <<<"$(mkvmerge -i $file)"
 }
 
+
 # Determine the sample rate for audio. If different tracks have different
-# rates, chooses that of the first audio track.
+# rates, choose that of the first audio track.
 function get_audio_sample_rate {
 	local file=$1
 	sample_rates=$(mkvinfo $file | grep -m1 "Sampling frequency")
-	audio_sample_rate=$(echo $sample_rates | egrep -o "[0-9]+\.[0-9]*")
+	audio_sample_rate=$(echo $sample_rates | egrep -o "[0-9]+\.?[0-9]*")
 }
+
 
 # Alter the audio sample rate. Copy video, subtitles, chapters exactly as they
 # are in the temp file.
@@ -124,6 +144,10 @@ function fix_audio {
 	-c:v copy -c:s copy -max_interleave_delta 0 $outfile
 }
 
+
+# Zhu Li, do the thing!
+
+check_diff $infile $outfile
 confirm_overwrite $outfile
 fix_chapters
 get_sync_flags $infile
@@ -133,5 +157,5 @@ get_sync_flags $infile
 # chapters from the new chapter file. Write everything to a temp file.
 mkvmerge --output $tempfile $syncstring --no-chapters $chapter_string $infile
 
-fix_audio
+fix_audio # This is the part that takes forever...
 cleanup
